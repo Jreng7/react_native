620e982422e52ae2f78cdce3fbe737b89a616016
Josue Ribeiro <jr.waydev@gmail.com>
2023-05-21 23:26:14 -0300

    alteração

--- vendas/.prettierrc.js
+++ vendas/.prettierrc.js
@@ -4,4 +4,26 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  singleQuote: true,
+  endOfLine: 'auto',
+};
+
+{
+  "prettier/prettier"; [
+    "error",
+    {
+      "singleQuote": true,
+      "parser": "flow",
+      "endOfLine": "auto"
+    }
+  ]
+}
+
+
+module.exports = {
+  "rules": {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
 };
