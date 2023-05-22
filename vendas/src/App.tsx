8379087eb3c5dd55aa24bbbbc01254dc629f26ee
Josue Ribeiro <jr.waydev@gmail.com>
2023-05-21 21:08:52 -0300

    react-native change

---
+++ vendas/src/App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Text>Mudanças na área</Text>
+        </SafeAreaView>
+    );
+};
+
+export default App;
